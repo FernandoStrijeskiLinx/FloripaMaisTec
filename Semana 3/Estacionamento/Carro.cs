4d26a69b9b1071bb85b1b16cb2aee04fe791844a
FernandoStrijeskiLinx <fernando.strijeski@linx.com.br>
2023-02-14 19:47:04 -0300

    classe carro ok

---
+++ Semana 3/Estacionamento/Carro.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Estacionamento
+{
+    public class Carro
+    {
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Marca { get; set; }
+        public List<Ticket> Tickets { get; set; }
+    }
+}
